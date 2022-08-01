8e163037ed885eda82a2fad268f54e42ffd2725c
kcl1s <lohmeyer.keith@gmail.com>
2022-08-01 07:21:57 -0400

    Module- save to modules directory on Raspberry Pi

--- LCD1602BD.py
+++ LCD1602BD.py
@@ -2,24 +2,24 @@ import smbus2 as smbus
 BUS = smbus.SMBus(1)
 ### added for big digits KCL
 custChar = [[31, 31, 31, 0, 0, 0, 0, 0],      # Small top line - 0
-			[0, 0, 0, 0, 0, 31, 31, 31],      # Small bottom line - 1
-			[31, 31, 0, 0, 0, 0, 31, 31],       # Small lines top and bottom -2
-			[0, 0, 0, 0, 0, 0,  31, 31],       # Thin bottom line - 3
-			[31, 31, 31, 31, 31, 31, 15, 7],  # Left bottom chamfer full - 4
-			[28, 30, 31, 31, 31, 31, 31, 31], # Right top chamfer full -5
-			[31, 31, 31, 31, 31, 31, 30, 28], # Right bottom chamfer full -6
-			[7, 15, 31, 31, 31, 31, 31, 31]]  # Left top chamfer full -7
+		[0, 0, 0, 0, 0, 31, 31, 31],      # Small bottom line - 1
+		[31, 31, 0, 0, 0, 0, 31, 31],       # Small lines top and bottom -2
+		[0, 0, 0, 0, 0, 0,  31, 31],       # Thin bottom line - 3
+		[31, 31, 31, 31, 31, 31, 15, 7],  # Left bottom chamfer full - 4
+		[28, 30, 31, 31, 31, 31, 31, 31], # Right top chamfer full -5
+		[31, 31, 31, 31, 31, 31, 30, 28], # Right bottom chamfer full -6
+		[7, 15, 31, 31, 31, 31, 31, 31]]  # Left top chamfer full -7
 
 bigNums = [ [7, 0, 5, 4, 1, 6],         #0
-			[0, 5, 254, 1, 255, 1],     #1
-			[0, 2, 5, 7, 3, 1],         #2
-			[0, 2, 5, 1, 3, 6],         #3
-			[7, 3, 255, 254, 254, 255], #4
-			[255, 2, 0, 1, 3, 6],       #5
-			[7, 2, 0, 4, 3, 6],         #6
-			[0, 0, 5, 254, 7, 254],     #7
-			[7, 2, 5, 4, 3, 6],         #8
-			[7, 2, 5, 1, 3, 6]]         #9
+		[0, 5, 254, 1, 255, 1],     #1
+		[0, 2, 5, 7, 3, 1],         #2
+		[0, 2, 5, 1, 3, 6],         #3
+		[7, 3, 255, 254, 254, 255], #4
+		[255, 2, 0, 1, 3, 6],       #5
+		[7, 2, 0, 4, 3, 6],         #6
+		[0, 0, 5, 254, 7, 254],     #7
+		[7, 2, 5, 4, 3, 6],         #8
+		[7, 2, 5, 1, 3, 6]]         #9
 
 def write_word(addr, data):
 	global BLEN
